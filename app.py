acdcae9044c1016696b2eb92ae2bbbd5387b614d
Shruti Sahu <124419362+shhruutiii@users.noreply.github.com>
2023-03-18 00:30:52 +0000

    added link

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app=Flask(__name__)
 @app.route("/")
 def hello_world():
   return render_template("home.html")
+  return render_template("q1.py")
 
 if __name__== "__main__":
   app.run(host="0.0.0.0",debug=True)
